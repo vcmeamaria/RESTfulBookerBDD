be38f158b3cdee59a1f4f7ade649f1c9915fbf9c
Maria <115981159+vcmeamaria@users.noreply.github.com>
2026-09-08 09:56:53 +0100

    Add RestSharp API client and Serilog logging

---
+++ RestfulBooker.Tests/ApiSmokeTest.cs
@@ -0,0 +1,19 @@
+﻿using RestfulBooker.Tests.Api;
+
+namespace RestfulBooker.Tests;
+
+[TestFixture]
+public sealed class ApiSmokeTest
+{
+    [Test]
+    public async Task PingEndpointShouldBeAvailable()
+    {
+        var api = new BookerApiClient();
+
+        var response = await api.Ping();
+
+        Assert.That(
+            (int)response.StatusCode,
+            Is.EqualTo(201));
+    }
+}
